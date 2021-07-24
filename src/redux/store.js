f0006807ae6be66474a9ab544bb5bcffd667b84d
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-07-25 02:27:43 +0300

    Отключил localStorage в проекте

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import contactsReducer from './contacts/contacts-reducer';
-import middleware from './middleware';
+import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+import contactsReducer from "./contacts/contacts-reducer";
+import middleware from "./middleware";
 
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
+// const contactsPersistConfig = {
+//   key: 'contacts',
+//   storage,
+//   blacklist: ['filter'],
+// };
 
 const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    contacts: contactsReducer,
   },
   middleware,
-  devTools: process.env.NODE_ENV === 'development',
+  devTools: process.env.NODE_ENV === "development",
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
 
 export default store;
